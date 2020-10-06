cacf470c5588c3b622ab0fd0f4bc982a3e68c3b0
mehuldudhane6 <65207452+mehuldudhane6@users.noreply.github.com>
2020-10-06 18:46:53 +0530

    Add files via upload

---
+++ AssignmentQ53.java
@@ -0,0 +1,17 @@
+public class Q53 {
+
+	public static void main(String[] args) {
+		String s1 ="67, 89, 23, 67, 12, 55, 66";
+		String [] str = s1.split(", ");
+		int a[]=new int[str.length];
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+			a[i] = Integer.parseInt(str[i]);
+			sum=sum+a[i];
+		}
+		System.out.println("Sum = "+sum);
+
+	}
+
+}
